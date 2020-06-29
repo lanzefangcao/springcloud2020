65d770f00b2d120d2e80dc812a379209767ae36c
兰泽 <64194152+lanzefangcao@users.noreply.github.com>
2020-06-29 15:24:47 +0800

    Update ConfigCenterMain3344.java

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -1,6 +1,5 @@
 package com.atguigu.springcloud;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -10,6 +9,5 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344 .class,args);
-
     }
 }
